c984d7a663bdbd8e6880f43422dd2e78f0ca1c7c
Muhamad Nauval Azhar <nauvalazhar2@gmail.com>
2019-12-13 22:32:12 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Pagination\Paginator;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         URL::forceSchema('https');
-        
+
         \Carbon\Carbon::setLocale(config('app.locale'));
 
         Blade::component('components.button');
