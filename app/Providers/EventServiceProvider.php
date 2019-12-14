06332d0e5f44cb8c7396fa1277fa7cb9ed7b9766
Rizal Fakhri <me@rizalfakhri.id>
2019-12-14 23:41:32 +0800

    Register events & listeners for the DiscoverPost

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\Events\Post\{
+    Discover\DiscoverPostCreated,
+};
+use App\Listeners\Post\{
+    Discover\FetchDiscoverPostUrlPreview,
+};
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +24,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        DiscoverPostCreated::class => [
+            FetchDiscoverPostUrlPreview::class
+        ]
     ];
 
     /**
