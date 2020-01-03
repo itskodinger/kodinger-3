939de1466424e5b65f483440a79a30380bfc8855
Muhamad Nauval Azhar <nauvalazhar2@gmail.com>
2020-01-04 02:19:03 +0700

    user dropdown full screen on mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,7 +66,7 @@
                         @else
                             <div class="relative flex-shrink-0">
                                 <a onclick="this.nextElementSibling.classList.toggle('hidden');"><img alt="{{ auth()->user()->name }}" src="{{ auth()->user()->the_avatar_sm }}" class="user-dropdown cursor-pointer rounded border w-10"></a>
-                                <ul class="user-dropdown-menu absolute hidden bg-white shadow-lg w-56 right-0 mt-3 rounded">
+                                <ul class="user-dropdown-menu sm:absolute fixed sm:h-auto h-full hidden bg-white shadow-lg w-56 right-0 sm:mt-3 mt-4 sm:rounded sm:w-auto w-full">
                                     @can('dashboard')
                                     <li><a class="flex py-3 px-5 text-sm text-indigo-600 hover:bg-indigo-100 rounded-tr rounded-tl" href="{{ route('dashboard.index') }}">
                                         <svg class="w-4 mr-3 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="home"><rect width="24" height="24" opacity="0"/><path d="M20.42 10.18L12.71 2.3a1 1 0 0 0-1.42 0l-7.71 7.89A2 2 0 0 0 3 11.62V20a2 2 0 0 0 1.89 2h14.22A2 2 0 0 0 21 20v-8.38a2.07 2.07 0 0 0-.58-1.44zM10 20v-6h4v6zm9 0h-3v-7a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H5v-8.42l7-7.15 7 7.19z"/></g></g></svg>
