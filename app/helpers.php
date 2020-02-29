34a3dad713daa0441d8d4608267208330eb99f49
Nauval <nauvalazhar2@gmail.com>
2020-02-29 15:27:06 +0700

    added file name sanitazier

--- app/helpers.php
+++ app/helpers.php
@@ -375,3 +375,18 @@ function can($str)
 {
 	return auth()->user()->can($str);
 }
+
+function safe_file_name($string, $force_lowercase=true, $anal=false)
+{
+    $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
+                   "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
+                   "â€”", "â€“", ",", "<", ".", ">", "/", "?");
+    $clean = trim(str_replace($strip, "", strip_tags($string)));
+    $clean = preg_replace('/\s+/', "-", $clean);
+    $clean = ($anal) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
+    return ($force_lowercase) ?
+        (function_exists('mb_strtolower')) ?
+            mb_strtolower($clean, 'UTF-8') :
+            strtolower($clean) :
+        $clean;
+}
