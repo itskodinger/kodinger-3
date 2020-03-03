aaeee27a1bf57c023b286545641de3e8620647ba
Nauval <nauvalazhar2@gmail.com>
2020-03-03 07:44:17 +0700

    Added auth logic

--- resources/views/layouts/useralert.blade.php
+++ resources/views/layouts/useralert.blade.php
@@ -1,7 +1,9 @@
 @inject('contributeService', 'Services\ContributeService')
 
+@auth
 @if(count($contributeService->onMyPostsDraft()) > 0)
 	<div class="py-4 px-6 mb-4 bg-blue-100 text-blue-600 border-2 border-blue-200 rounded text-sm leading-loose">
 		Terdapat {{ count($contributeService->onMyPostsDraft()) }} kontribusi pada post kamu – tinjau <a class="pb-1 border-b border-blue-600" href="@route('contribute.myposts')">di sini</a>
 	</div>
 @endif
+@endauth
