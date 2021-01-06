59af18046f3595a95d16680346b608a3be787b21
rizalfakhri <me@rizalfakhri.id>
2021-01-06 23:43:02 +0800

    Fix redirector & embed

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.app', ['title' => 'Lanjutkan Link'])
 
 @section('content')
-    <div class="container mx-auto">
-        <div class="flex py-12 -mx-6 justify-center">
-            <div class="w-6/12 px-6">
+    <div class="container px-6 mx-auto">
+        <div class="flex justify-center py-12 -mx-6">
+            <div class="w-full px-6 lg:w-6/12">
             	<form method="post" id="form">
 	            	<div class="p-8 bg-white border border-gray-200 rounded">
 	            		<div>
-		            		<h4 class="text-lg font-bold text-gray-600 border-b border-gray-200 pb-4 mb-6">Mengalihkan</h4>
+		            		<h4 class="pb-4 mb-6 text-lg font-bold text-gray-600 border-b border-gray-200">Mengalihkan</h4>
 
-                            <div class="bg-blue-100 border-l-4 border-blue-500 text-blue-700 px-4 py-2 mb-4" role="alert">
+                            <div class="px-4 py-2 mb-4 text-blue-700 bg-blue-100 border-l-4 border-blue-500" role="alert">
                                 <p class="font-bold">Perhatian</p>
                                 <p>Halaman di bawah ini tidak berafiliasi dengan Kodinger, dan mungkin mengandung konten yang berbahaya.</p>
                             </div>
 
 		            		<p class="mb-2 text-gray-600">Halaman Terkait</p>
-                            <div class="flex items-center bg-gray-100 border border-gray-200 mb-8 p-3 rounded">
-		            			<div class="bg-cover w-16 h-16 rounded" style="background-image: url({{ isset($redirectData['preview']) ? $redirectData['preview']->image : '' }});"></div>	
-		            			<div class="ml-4">
+                            <div class="flex flex-wrap items-center p-3 mb-8 bg-gray-100 border border-gray-200 rounded">
+		            			<div class="w-full h-32 bg-cover bg-center rounded lg:h-16 lg:w-16" style="background-image: url({{ isset($redirectData['preview']) ? $redirectData['preview']->image : '' }});"></div>
+		            			<div class="mt-2 lg:ml-4 lg:mt-0">
 		            				<h4 class="font-semibold text-indigo-600">{{ isset($redirectData['preview']) ? $redirectData['preview']->title : truncate($redirectData['url'], 50) }}</h4>
-		            				<p class="text-gray-600 mt-1">{{ isset($redirectData['preview']) ? truncate($redirectData['preview']->description, 60) : '' }}</p>
+		            				<p class="mt-1 text-gray-600">{{ isset($redirectData['preview']) ? truncate($redirectData['preview']->description, 60) : '' }}</p>
 
 		            			</div>
 		            		</div>
@@ -33,7 +33,7 @@
 							@endbutton
 
 							<div class="text-center">
-								<a href="{{ url()->previous() }}" class="text-gray-600 mt-5 hover:text-indigo-600 inline-block">Kembali</a>
+								<a href="{{ url()->previous() }}" class="inline-block mt-5 text-gray-600 hover:text-indigo-600">Kembali</a>
 							</div>
 						</div>
 	            	</div>
