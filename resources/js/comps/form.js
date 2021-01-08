4e8baeec6d5801f439b69880b9bdc1bd0beedbdb
rizalfakhri <me@rizalfakhri.id>
2021-01-09 07:16:41 +0800

    Add esc key to escape maximized editor

--- resources/js/comps/form.js
+++ resources/js/comps/form.js
@@ -530,6 +530,13 @@ class Form extends Component {
 
             }
 
+            if( e.keyCode == '27' && this.state.editorMaximized) {
+                e.preventDefault();
+
+                document.getElementById('maximizeButton').click();
+
+            }
+
 		});
 	}
 
@@ -2269,7 +2276,7 @@ class Form extends Component {
                                                                 <span>Unggah Gambar</span>
                                                             </button>
 
-                                                            <button onClick={this.toggleEditorMaximizer.bind(this)} className="flex items-center px-4 py-2 ml-1 bg-white border border-gray-300 rounded hover:border-indigo-500 hover:text-indigo-500">
+                                                            <button onClick={this.toggleEditorMaximizer.bind(this)} id="maximizeButton" className="flex items-center px-4 py-2 ml-1 bg-white border border-gray-300 rounded hover:border-indigo-500 hover:text-indigo-500">
                                                                 <svg className="ml-auto" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" /></svg>
                                                             </button>
 
